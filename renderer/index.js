f992b5dce71f317b1eef9edcc460cd3c14f25338
August Toman-Yih <august.toman.yih@gmail.com>
2018-10-24 23:02:15 -0700

    add canvas but i guess they're both slow dang

--- renderer/index.js
+++ renderer/index.js
@@ -4,6 +4,37 @@ import {ipcRenderer} from 'electron';
 
 const colorFromChannel = channel => `hsl(${(channel * 20)%100}, 100%, 80%)`;
 
+class MidiGraph extends React.Component {
+  componentDidMount() {
+    this.ctx = this.canvas.getContext('2d')
+    // this.ctx.begin
+  }
+
+  componentDidUpdate() {
+    // ctx.clearRect(0, 100)
+    Object.entries(this.props.messages).forEach(([channel, messages]) => {
+      const path = new Path2D(
+        messages
+          .map(({value}) => value)
+          .map((val, i) => `${i === 0 ? 'M' : 'L'} ${i} ${val}`).join(' ')
+      );
+
+      this.ctx.strokeStyle = colorFromChannel(channel);
+      this.ctx.stroke(path);
+    })
+  }
+
+  render() {
+    return (
+      <canvas {...{
+        ref: canvas => this.canvas = canvas,
+
+      }}/>
+    )
+  }
+}
+
+
 const MidiPath = ({messages, color}) => (
   <path d={
     messages
@@ -30,11 +61,14 @@ class Listen extends React.Component {
   render() {
     return (
       <div>
-        <svg viewBox='0 0 200 127'>
-          {
-            Object.entries(this.state).map(([channel, messages]) => <MidiPath messages={messages} color={colorFromChannel(channel)}/>)
-          }
-        </svg>
+        {/*<svg viewBox='0 0 200 127'>*/}
+          {/*{*/}
+            {/*Object.entries(this.state).map(([channel, messages]) => <MidiPath messages={messages} color={colorFromChannel(channel)}/>)*/}
+          {/*}*/}
+        {/*</svg>*/}
+        <MidiGraph {...{
+          messages: this.state
+        }}/>
         <div>
           {JSON.stringify(this.state)}
         </div>
