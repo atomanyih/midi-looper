2814ae056c9e175618dc71241fec76fc39151161
August Toman-Yih <august.toman.yih@gmail.com>
2018-10-24 22:43:20 -0700

    svg shows many midi but really slow

--- renderer/index.js
+++ renderer/index.js
@@ -2,36 +2,42 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import {ipcRenderer} from 'electron';
 
+const colorFromChannel = channel => `hsl(${(channel * 20)%100}, 100%, 80%)`;
+
+const MidiPath = ({messages, color}) => (
+  <path d={
+    messages
+      .map(({value}) => value)
+      .map((val, i) => `${i === 0 ? 'M' : 'L'} ${i} ${val}`).join(' ')
+  } stroke={color} fill='none'/>
+)
 
 class Listen extends React.Component {
   state = {
-    midiMessages: []
   };
+
   componentDidMount() {
-    ipcRenderer.on('midi-msg', (event,msg) => {
-      let newArray = [...this.state.midiMessages, msg];
-      if(newArray.length > 200) {
-        newArray = newArray.slice(newArray.length - 200)
-      }
+    ipcRenderer.on('midi-msg', (event, msg) => {
+      const {channel} = msg;
+      const oldArray = this.state[channel] || [];
+      const newArray = [...oldArray, msg];
       this.setState({
-        midiMessages: newArray
+        [channel]: newArray
       });
     });
   }
 
   render() {
-    const path = this.state.midiMessages
-      .map(({value}) => value)
-      .map((val, i) => `${i === 0 ? 'M' : 'L'} ${i} ${val}`).join(' ');
-
     return (
       <div>
-        <svg viewBox='0 0 200 127' stroke='white' fill='none'>
-          <path d={path}/>
+        <svg viewBox='0 0 200 127'>
+          {
+            Object.entries(this.state).map(([channel, messages]) => <MidiPath messages={messages} color={colorFromChannel(channel)}/>)
+          }
         </svg>
-        {/*<div>*/}
-          {/*{JSON.stringify(this.state)}*/}
-        {/*</div>*/}
+        <div>
+          {JSON.stringify(this.state)}
+        </div>
       </div>
     )
   }
